f215db320af2c382143603d128b38f8066e2931a
Ray Hernandez <107069990+hernandezraymondm@users.noreply.github.com>
2024-12-11 23:25:24 +0800

    update error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { config } from "./config/app.config";
 import connectDatabase from "./database/modules/database";
 import { errorHandler } from "./middlewares/errorHandler";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -20,11 +22,14 @@ app.use(
 
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Hello and Welcome to Advance Auth API!!!",
-  });
-});
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Hello and Welcome to Advance Auth API!!!",
+    });
+  })
+);
 
 app.use(errorHandler);
 
--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { ErrorRequestHandler, Response } from "express";
 import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../common/utils/AppError";
 // import { AppError } from "../common/utils/AppError";
 // import {
 //   clearAuthenticationCookies,
@@ -40,12 +41,12 @@ export const errorHandler: ErrorRequestHandler = (
   //   return formatZodError(res, error);
   // }
 
-  // if (error instanceof AppError) {
-  //   return res.status(error.statusCode).json({
-  //     message: error.message,
-  //     errorCode: error.errorCode,
-  //   });
-  // }
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      errorCode: error.errorCode,
+    });
+  }
 
   return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
     message: "Internal Server Error",
